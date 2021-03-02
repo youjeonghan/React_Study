b27574f1fcc5d730deedc76680e7620d5a62cedc
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-03-03 01:55:07 +0900

    11. 배열 렌더링하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Hello from './Hello';
 import Counter from './Counter'
 import InputSample from './InputSample'
+import UserList from './UserList';
 
 function App() {
     return (
         <>
-            <InputSample />
+            <UserList />
         </>
     );
 }
---
+++ begin-react/src/UserList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function User({ user }) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    );
+}
+
+function UserList() {
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
+        }
+    ];
+    // 동적인 배열 렌더링을 위해서 map함수 사용
+    // 배열은 key를 설정해야 오류가 안생긴다 (리액트 효율 측면)
+    return (
+        <div>
+            {users.map(user => (
+                <User user={user} key={user.id} />
+            ))}
+        </div>
+    );
+}
+
+export default UserList;
