98a62706b1d083e4020387e130de2d88dae0a476
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-03-03 01:13:46 +0900

    6. 조건부 렌더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,7 +3,10 @@ import Hello from './Hello';
 
 function App() {
     return (
-        <Hello name="유정란" />
+        <>
+            <Hello name="유정란" />
+            <Hello name="유정한" color="red" isSpecial={true} />
+        </>
     );
 }
 
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-function Hello(props){
-    return <div>안녕하세요~{props.name}</div>
+function Hello({ color, name, isSpecial }) {
+    return (
+        <div style={{ color }}>
+            {isSpecial ? <b>*</b> : null}
+            안녕하세요~{name}
+        </div>
+    )
 }
 export default Hello;
